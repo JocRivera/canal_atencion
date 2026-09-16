dff609757ea8268eddcea38df1ec1879b8897b8b
JocRivera <jose_mrivera@soy.sena.edu.co>
2026-09-16 11:35:46 -0500

    reserva-servicio

--- src/reserva-servicio/reserva-servicio.service.ts
+++ src/reserva-servicio/reserva-servicio.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ReservaServicioService {}
+export class ReservaServicioService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async obtenerServiciosDeReserva(reservaId: string) {
+        return this.prisma.db.orm.public.ReservaServicio
+            .where({ reservaId })
+            .all();
+    }
+
+    async agregarServicioAReserva(
+        reservaId: string,
+        servicioId: string,
+        cantidad: number,
+    ) {
+        const servicio = await this.prisma.db.orm.public.Servicio.first({
+            id: servicioId,
+        });
+
+        if (!servicio) {
+            throw new NotFoundException(
+                `No existe el servicio con el id ${servicioId}`,
+            );
+        }
+
+        return this.prisma.db.orm.public.ReservaServicio.create({
+            reservaId,
+            servicioId,
+            cantidad,
+            precio: servicio.precio,
+        });
+    }
+
+    async actualizarCantidadDeServicioEnReserva(
+        reservaId: string,
+        servicioId: string,
+        cantidad: number,
+    ) {
+        const reservaServicio =
+            await this.prisma.db.orm.public.ReservaServicio.first({
+                reservaId,
+                servicioId,
+            });
+
+        if (!reservaServicio) {
+            throw new NotFoundException(
+                `No existe el servicio con el id ${servicioId} en la reserva con el id ${reservaId}`,
+            );
+        }
+
+        return this.prisma.db.orm.public.ReservaServicio
+            .where({
+                reservaId,
+                servicioId,
+            })
+            .update({
+                cantidad,
+            });
+    }
+}
